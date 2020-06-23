5adc377f8e8ccc1e9a84f0e28b3e4fb23cae5247
Shreyansh Chordia <shreyanshchordia9@gmail.com>
2020-06-24 01:55:32 +0530

    Update tokenize.py

--- DataPrepUtils/tokenize.py
+++ DataPrepUtils/tokenize.py
@@ -4,6 +4,27 @@ def tokenize(str):
     return str.split(" ")
 
 # function to tokenize data directory
+def tokenize_dir(data):
+    dir = dict()
+
+    for __class__ in data.keys():
+        for __list__ in data[__class__]:
+            temp_list = list()
+            for string in __list__:
+                
+                # here we tokenize
+                temp_list.append(tokenize(string))
+                # tokenization and normalization can even be combined
+                # I just wanted to keep things more elaborate
+
+            if dir.get(__class__, 0) == 0:
+                dir[__class__] = [temp_list]
+            
+            else:
+                dir[__class__].append(temp_list)
+    
+    return dir
+
 
 '''
 Use normalize.py to normalize entire directory before tokenizing
@@ -36,23 +57,3 @@ When the structure of the data directory is somewhat like this:
   ]
 }
 '''
-def tokenize_dir(data):
-    dir = dict()
-
-    for __class__ in data.keys():
-        for __list__ in data[__class__]:
-            temp_list = list()
-            for string in __list__:
-                
-                # here we tokenize
-                temp_list.append(tokenize(string))
-                # tokenization and normalization can even be combined
-                # I just wanted to keep things more elaborate
-
-            if dir.get(__class__, 0) == 0:
-                dir[__class__] = [temp_list]
-            
-            else:
-                dir[__class__].append(temp_list)
-    
-    return dir
