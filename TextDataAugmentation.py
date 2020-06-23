acc32f8ee211c17ec2b0976ac3c67edb6ccfa2ef
Shreyansh Chordia <shreyanshchordia9@gmail.com>
2020-06-24 02:34:00 +0530

    Update TextDataAugmentation.py

--- TextDataAugmentation.py
+++ TextDataAugmentation.py
@@ -15,7 +15,6 @@ technique 2: Substituting random words with their synonyms:- (word_substitutor()
 '''
 
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
-# thanks to https://gist.github.com/sebleier/554280#gistcomment-2596130
 
 def unk_substitutor(tokenized_sentence, k=1):
     length = len(tokenized_sentence)
